d0e6f0b820d9122cbb600e2c8d05b03c4239781a
Jaja <jayonga3@gmail.com>
2026-08-21 14:11:05 +0200

    feat(main): create main to parser input reg expression

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+# include "Parser.hh"
+# include <iostream>
+# include <string>
+
+std::string enumToString(NodeType tp) {
+    switch(tp) {
+        case 0:
+            return "CHAR";
+            break;
+        case 1:
+            return "CONCAT";
+            break;
+        case 2:
+            return "ALT";
+            break;
+        case 3:
+            return "STAR";
+            break;
+        case 4:
+            return "PLUS";
+            break;
+        case 5:
+            return "QUESTION";
+            break;
+        case 6:
+            return "EMPTY";
+            break;
+        default:
+            return "";
+            break;
+    }
+}
+
+void printBT(const std::string& prefix, NodePtr& node, bool isLeft)
+{
+    if (node)
+    {
+        std::cout << prefix;
+        std::cout << (isLeft ? "├──" : "└──" );
+
+        // print the value of the node
+        std::cout << enumToString(node->type) << std::endl;
+
+        // enter the next tree level - left and right branch
+        printBT( prefix + (isLeft ? "│   " : "    "), node->left_operand, true);
+        printBT( prefix + (isLeft ? "│   " : "    "), node->right_operand, false);
+    }
+}
+
+void printBT(NodePtr& node) {
+    printBT("", node, false);    
+}
+
+int main () {
+    std::string s;
+    std::cin >> s;
+    Parser p(s);
+    NodePtr tree = p.parse();
+    printBT(tree);
+}
